846b18ebbeb78f3ceba434f8cce50795732092d5
jisulee42 <leejisu1584@gmail.com>
2022-07-13 23:54:26 +0900

    today commit

---
+++ 2week/elice_0713/elice.js
@@ -0,0 +1,11 @@
+const name = 'elice';
+const age = 5;
+const nationality = 'korea';
+
+
+module.exports = {
+    name,
+    age,
+    nationality
+}
+
---
+++ 2week/elice_0713/index.js
@@ -0,0 +1,9 @@
+const student = require('./elice');
+const dayy = require('dayjs');
+const jisu = require('./prac');
+
+console.log(student)
+console.log(dayy());
+console.log(jisu);
+
+    
